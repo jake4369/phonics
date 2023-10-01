bce8919921e903fa824877c33d156791908ee72e
Jake Samuels <samuels242@googlemail.com>
2023-10-01 21:26:24 +0100

    Header

--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -1,17 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header>
+    <header className="sticky-header">
       <Image
         src="/assets/logo.png"
         alt="logo"
         width={60}
         height={60}
         className="header__logo"
+        priority
       />
 
-      <button className="header__btn">Home</button>
+      <Link href="/" className="header__btn">
+        Home
+      </Link>
     </header>
   );
 };
