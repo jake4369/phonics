0554d64fbecef1a60724cb67ade790de18521c38
Jake Samuels <samuels242@googlemail.com>
2023-10-01 11:28:52 +0100

    Import Hero section

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
+import Hero from "@components/Homepage/Hero";
+
 const Home = () => {
-  return <main>Home</main>;
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
 };
 
 export default Home;
