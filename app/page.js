aad79dfb02de6aa3fbd76c1c0be55ba86400162b
Jake Samuels <samuels242@googlemail.com>
2023-10-01 21:25:14 +0100

    Homepage

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Hero from "@components/Homepage/Hero";
+import LearningSetSection from "@components/Homepage/LearningSetSection";
 
 const Home = () => {
   return (
     <main>
       <Hero />
+      <LearningSetSection />
     </main>
   );
 };
