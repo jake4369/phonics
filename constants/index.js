9e05febb5731c880fc61a052b3ebb649201cfa18
Jake Samuels <samuels242@googlemail.com>
2023-10-01 21:30:18 +0100

    Constants

---
+++ constants/index.js
@@ -0,0 +1,37 @@
+export const setOneLetters = [
+  {
+    group: "Group 1",
+    letters: ["s", "a", "t", "p"],
+  },
+  {
+    group: "Group 2",
+    letters: ["i", "n", "m", "d"],
+  },
+  {
+    group: "Group 3",
+    letters: ["g", "o", "c", "k"],
+  },
+  {
+    group: "Group 4",
+    letters: ["e", "u", "r", "h"],
+  },
+  {
+    group: "Group 5",
+    letters: ["b", "f, ff", "l, ll", "ss"],
+  },
+  {
+    group: "Special Friends",
+    letters: ["sh", "th", "ch", "qu", "ng", "nk", "ck"],
+  },
+];
+
+export const setTwoLetters = [
+  { group: "Group 1", letters: ["ay", "ee", "igh", "ow", "oo"] },
+  { group: "Group 2", letters: ["oo", "ar", "or", "air", "ir", "ou", "oy"] },
+];
+
+export const setThreeLetters = [
+  { group: "Group 1", letters: ["ea", "oi", "a-e", "i-e", "o-e", "u-e"] },
+  { group: "Group 2", letters: ["aw", "are", "ur", "er", "ow", "ai"] },
+  { group: "Group 3", letters: ["oa", "ew", "ire", "ear", "ure"] },
+];
