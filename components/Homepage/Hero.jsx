10414a0c1ea49aef980407a010e487e0339656bc
Jake Samuels <samuels242@googlemail.com>
2023-10-01 11:29:53 +0100

    Hero section

---
+++ components/Homepage/Hero.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <section className="hero-section">
+      <Image
+        src="/assets/images/homepage/banner.png"
+        alt="Phonics"
+        width={300}
+        height={200}
+        className="hero-section__img"
+      />
+
+      <h1>Welcome to My Phonics</h1>
+    </section>
+  );
+};
+
+export default Hero;
