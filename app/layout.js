02697b3f671e437928f5302505332e754676d3ba
Jake Samuels <samuels242@googlemail.com>
2023-10-01 11:15:58 +0100

    Import Header

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,19 @@
 import "@styles/index.css";
 
+import Header from "@components/Layout/Header";
+
 export const metadata = {
-  title: "Phonics",
+  title: "My Learning - Phonics",
   description: "Let's Learn Phonics",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
