0259a4dfc973fd0175c13deb2959cfa1bd1ad23a
Jake Samuels <samuels242@googlemail.com>
2023-10-03 11:08:43 +0100

    Conditionally render letters

--- components/SpeedSoundPage/Hero.jsx
+++ components/SpeedSoundPage/Hero.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Hero = ({ letter, pronunciation, color }) => {
   return (
     <section className="speed-sound__letter-page__hero">
-      <h1 style={{ color }}>{letter}</h1>
+      <h1 style={{ color }}>
+        {letter === "ff" ? "f, ff" : letter === "ll" ? "l, ll" : letter}
+      </h1>
 
       <p>{pronunciation}</p>
     </section>
