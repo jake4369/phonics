c838bbb5230aa846c8c5f3bdb2bb9de7838dedbe
Jake Samuels <samuels242@googlemail.com>
2023-10-03 11:08:12 +0100

    Fix href

--- components/SetSection/LetterLink.jsx
+++ components/SetSection/LetterLink.jsx
@@ -3,13 +3,19 @@ import Link from "next/link";
 const LetterLink = ({ set, letter, backgroundColor }) => {
   return (
     <Link
-      href={`/learning-set/${set}/speed-sounds/${letter}`}
+      href={
+        letter === "ff"
+          ? `/learning-set/${set}/speed-sounds/ff`
+          : letter === "ll"
+          ? `/learning-set/${set}/speed-sounds/ll`
+          : `/learning-set/${set}/speed-sounds/${letter}`
+      }
       className="letter-link"
       style={{
         backgroundColor,
       }}
     >
-      {letter}
+      {letter === "ff" ? "f, ff" : letter === "ll" ? "l, ll" : letter}
     </Link>
   );
 };
